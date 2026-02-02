6107c47b08de9def2d22f95b85e65ec2f91364d5
Harshil182 <prajapatiharshil251@gmai.com>
2026-02-02 15:42:08 +0530

    change bg-colour

--- 04bgcolor/src/App.jsx
+++ 04bgcolor/src/App.jsx
@@ -1,18 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colour, setcolour] = useState("black");
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div
+        className="w-full h-screen duration-200 "
+        style={{ backgroundColor: colour }}
+      >
+        <div className="fixed flex flex-wrap justify-center inset-x-0 bottom-12 px-2">
+          <div className="flex flex-wrap justify-center shadow-lg gap-3 bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setcolour("red")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "Red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setcolour("green")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "green" }}
+            >
+              green
+            </button>
+            <button
+              onClick={() => setcolour("blue")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "blue" }}
+            >
+              blue
+            </button>
+            <button
+              onClick={() => setcolour("Orange")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "Orange" }}
+            >
+              Orange
+            </button>
+            <button
+              onClick={() => setcolour("yellow")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "yellow" }}
+            >
+              yellow
+            </button>
+            <button
+              onClick={() => setcolour("#525252")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "#525252" }}
+            >
+              gray
+            </button>
+            <button
+              onClick={() => setcolour("white")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "white" }}
+            >
+              white
+            </button>
+            <button
+              onClick={() => setcolour("olive")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "olive" }}
+            >
+              olive
+            </button>
+            <button
+              onClick={() => setcolour("purple")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "purple" }}
+            >
+              purple
+            </button>
+            <button
+              onClick={() => setcolour("pink")}
+              className=" outline-none px-4 py-1 rounded-full shadow-sm  "
+              style={{ backgroundColor: "darkpink" }}
+            >
+              pink
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
